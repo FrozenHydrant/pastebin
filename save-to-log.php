a8063473d8fe19eadbf90e00425ed60061b10ef1
FrozenHydrant <richard.you.777@gmail.com>
2019-02-18 14:45:38 -0800

    Ok so PHP isn't allowed, lets try this

--- save-to-log.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $log_file_name = 'mylog.txt'; // Change to the log file name
-  $message = $_POST['message']; // incoming message
-  file_put_contents($log_file_name, $message);
-  header('Location: /');
-?>
